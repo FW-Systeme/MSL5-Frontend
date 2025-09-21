2efa43e7b0068b45c74cb426f6f53a4a31035e36
fonzane <timschaffert@yahoo.de>
2025-09-21 15:23:27 +0400

    working on displaying device information

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -2,18 +2,25 @@ import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { Device } from './device.interface';
 import { DeviceService } from './device.service';
 import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-device',
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MatDividerModule,
+    MatListModule,
   ],
   templateUrl: './device.component.html',
   styleUrl: './device.component.scss'
 })
 export class DeviceComponent implements OnInit {
 
-  deviceInfo: WritableSignal<Device | null> = signal(null);
+  // deviceInfo: WritableSignal<Device | null> = signal(null);
+  deviceInfo: Device | null = null;
 
   constructor(
     private deviceService: DeviceService
@@ -21,8 +28,10 @@ export class DeviceComponent implements OnInit {
 
   async ngOnInit() {
     let device = await this.deviceService.getDeviceData();
-    if (device) this.deviceInfo.set(device as Device);
-    console.log(this.deviceInfo());
+    // if (device) this.deviceInfo.set(device as Device);
+    // console.log(this.deviceInfo());
+    if (device) this.deviceInfo = device;
+    console.log(this.deviceInfo);
   }
 
 }
